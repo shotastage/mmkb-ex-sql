5d4be521e539e795b6b1bacf1892e75236d36e6a
Shota Shimazu <shotastage@users.noreply.github.com>
2021-06-29 16:25:18 +0900

    revise

--- ex-2.sql
+++ ex-2.sql
@@ -1,4 +1,13 @@
 -- PSQL Script --
+
+-- INITIALIZE TABLES --
+DROP TABLE IF EXISTS stations;
+DROP TABLE IF EXISTS place;
+DROP TABLE IF EXISTS railway_relations;
+
+
+
+-- DECLARED FUNCTIONS --
 CREATE OR REPLACE FUNCTION gis_distance(point, point)
 RETURNS double precision AS
 $BODY$
@@ -20,27 +29,75 @@ FROM (SELECT $1[0] AS lat, $1[1] AS lng) AS l1
 $BODY$
 LANGUAGE sql;
 
--- INITIALIZE TABLES --
-DROP TABLE IF EXISTS stations;
-DROP TABLE IF EXISTS place;
+
+
+CREATE OR REPLACE FUNCTION create_tranfer() RETURNS "trigger" AS
+$BODY$
+declare
+    transfer text[];
+begin
+    
+    SELECT array_agg(c2.sid) INTO transfer FROM stations c2
+    WHERE c2.sid = new.sid AND gis_distance(new.location, c2.location) < 0.25 GROUP BY gis_distance(new.location, c2.location);
+
+
+    INSERT INTO railway_relations VALUES (new.id, new.sid, transfer);
+return new;
+end;
+$BODY$
+LANGUAGE plpgsql;
 
 
 -- STATION TABLE--
 CREATE TABLE stations (
-    id text,　name text, location point
+    id text,
+    sid text,
+    name text,
+    location point
 );
-INSERT INTO stations VALUES ('1','KANNAI','(35.444136, 139.635981)');
-INSERT INTO stations VALUES ('2','SAKURAGICHO','(35.450840, 139.631136)');
-INSERT INTO stations VALUES ('3','YOKOHAMA','(35.466024, 139.622677)');
-INSERT INTO stations VALUES ('4','TAKASHIMACHO','(35.459080, 139.623374)');
-INSERT INTO stations VALUES ('5','B-KANNAI','(35.445917, 139.635683)');
-INSERT INTO stations VALUES ('6','ISEZAKI-CHOJYAMACHI','(35.441044, 139.633008)');
-INSERT INTO stations VALUES ('7','SHIN-TAKASHIMA','(35.462043, 139.626582)');
-INSERT INTO stations VALUES ('8','MINATOMIRAI','(35.457209, 139.632966)');
-INSERT INTO stations VALUES ('9','BASHAMICHI','(35.449966, 139.636613)');
-INSERT INTO stations VALUES ('10','NIHONOODORI','(35.446759, 139.642711)');
-INSERT INTO stations VALUES ('11','MOTOMACHI-CHUKAGAI','(35.442296, 139.650651)');
-INSERT INTO stations VALUES ('12','ISHIKAWA-CHO','(35.438745, 139.642987)');
+
+INSERT INTO stations VALUES ('1', 'K1', 'KANNAI', '(35.444136, 139.635981)');
+INSERT INTO stations VALUES ('2', 'S1', 'SAKURAGICHO', '(35.450840, 139.631136)');
+INSERT INTO stations VALUES ('3', 'Y1', 'YOKOHAMA', '(35.466024, 139.622677)');
+INSERT INTO stations VALUES ('4', 'T1', 'TAKASHIMACHO', '(35.459080, 139.623374)');
+INSERT INTO stations VALUES ('5', 'K1', 'B-KANNAI', '(35.445917, 139.635683)');
+INSERT INTO stations VALUES ('6', 'I1', 'ISEZAKI-CHOJYAMACHI', '(35.441044, 139.633008)');
+INSERT INTO stations VALUES ('7', 'S1', 'SHIN-TAKASHIMA', '(35.462043, 139.626582)');
+INSERT INTO stations VALUES ('8', 'M1', 'MINATOMIRAI', '(35.457209, 139.632966)');
+INSERT INTO stations VALUES ('9', 'B1', 'BASHAMICHI', '(35.449966, 139.636613)');
+INSERT INTO stations VALUES ('10','N1', 'NIHONOODORI', '(35.446759, 139.642711)');
+INSERT INTO stations VALUES ('11','M1', 'MOTOMACHI-CHUKAGAI', '(35.442296, 139.650651)');
+INSERT INTO stations VALUES ('12','I2', 'ISHIKAWA-CHO', '(35.438745, 139.642987)');
+
+CREATE TRIGGER create_tranfer
+  AFTER INSERT
+  ON stations
+  FOR EACH ROW
+  EXECUTE PROCEDURE create_tranfer();
+
+
+
+CREATE TABLE railway_relations (
+    id text,
+    sid text, 
+    transfer text[]
+);
+
+
+
+
+INSERT INTO stations VALUES ('1', 'K1', 'KANNAI');
+INSERT INTO stations VALUES ('2', 'S1', 'SAKURAGICHO');
+INSERT INTO stations VALUES ('3', 'Y1', 'YOKOHAMA');
+INSERT INTO stations VALUES ('4', 'T1', 'TAKASHIMACHO');
+INSERT INTO stations VALUES ('5', 'K1', 'B-KANNAI');
+INSERT INTO stations VALUES ('6', 'I1', 'ISEZAKI-CHOJYAMACHI');
+INSERT INTO stations VALUES ('7', 'S1', 'SHIN-TAKASHIMA');
+INSERT INTO stations VALUES ('8', 'M1', 'MINATOMIRAI');
+INSERT INTO stations VALUES ('9', 'B1', 'BASHAMICHI');
+INSERT INTO stations VALUES ('10','N1', 'NIHONOODORI');
+INSERT INTO stations VALUES ('11','M1', 'MOTOMACHI-CHUKAGAI');
+INSERT INTO stations VALUES ('12','I2', 'ISHIKAWA-CHO');
 
 
 
@@ -78,32 +135,3 @@ ORDER BY distance ASC;
 SELECT c2.*, gis_distance(c1.location, c2.location) AS distance FROM stations c1, place c2
 WHERE c1.name = 'MINATOMIRAI' AND c2.type = 'sightseeing'
 ORDER BY distance ASC;
-
-
-CREATE FUNCTION div_proc() RETURNS trigger AS $div_proc$
-BEGIN
-    INSERT INTO answer VALUES (NEW.id, div(NEW.operand1, NEW.operand2), mod(NEW.operand1, NEW.operand2));  
-    RETURN NEW;
-END;
-$div_proc$
-LANGUAGE plpgsql;
-
-
--- https://www.google.co.jp/maps/place/山下公園/@35.4474657,139.6151589,13.51z/data=!4m5!3m4!1s0x60185ce2b1bb57b7:0xe3ba49f16272376c!8m2!3d35.4456543!4d139.6500049?hl=ja
-
-SELECT TRANSLATE(gmapurl,'https://www.google.co.jp/maps/place/','')
-DROP FUNCTION IF EXISTS gmap_convert_trigger();
-CREATE FUNCTION gmap_convert_trigger() RETURNS trigger AS '
-  BEGIN
-    update trigger_test_table set arrival = arrival + 1;
-    delete from trigger_test_table where arrival >= 10;
-    delete from target1 where arrival = 0;
-    insert into target1 (title, arrival, date)
-       select movie.title, arrival, date
-       from movie, trigger_test_table
-       where trigger_test_table.title = movie.title
-       and arrival = 1;
-    return new;
-  END;
-'
-language 'plpgsql';
